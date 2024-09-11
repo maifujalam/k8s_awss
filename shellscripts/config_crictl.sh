f881ec54a267bbbbd720b86619e5d8869ee8ef39
maifujalam <sk.maifujalam@gmail.com>
2024-09-11 20:02:46 +0530

    initt commit

---
+++ shellscripts/config_crictl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+PROJECT_PATH=~/k8s_aws
+
+printf "\nConfiguring crictl\n"
+#### Download crictl #######
+#VERSION="v1.30.1"
+#if [[ ! -f  /vagrant/ShellScripts/crictl-$VERSION-linux-amd64.tar.gz ]];then
+#  printf "Downloading crictl binary"
+#  wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
+#fi
+#sudo tar zxf /vagrant/ShellScripts/crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
+############ Configure crictl ##########################
+sudo cp $PROJECT_PATH/k8s/v1.30.4/configs/config.toml /etc/containerd/ -vf
+sudo systemctl restart containerd.service
+#sudo chmod -R 777 /var/run/containerd/
+sudo cp $PROJECT_PATH/k8s/v1.30.4/configs/crictl.yaml /etc/ -vf
+crictl version
+printf "\nChecking All containers..\n\n"
+crictl ps
