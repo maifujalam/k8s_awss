b9f512abfac4ccaa142f6b3499d4fdf437f3ff73
maifujalam <sk.maifujalam@gmail.com>
2024-09-11 22:24:12 +0530

    init commit

--- shellscripts/3-configure_firealld_selinux.sh
+++ shellscripts/3-configure_firealld_selinux.sh
@@ -5,9 +5,10 @@ sudo setenforce 0
 #sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 sudo sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
 ########## Disable Swap ##########
-printf "Diableling SWAP and FIREWALL ..."
+printf "Diableling SWAP and FIREWALL ...\n"
 swapoff -a
 sed -i.bak '/swap/ s/^/#/' /etc/fstab
 # Disable swap permanently
-sudo systemctl disable --now firewalld
+
+sudo systemctl disable --now ufw
 printf "\n\nFirewall and Selinux config Completed.\n\n"
