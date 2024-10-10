ea5e65d438451e297a9e8cbe8226674a4ac1ddbe
maifujalam <sk.maifujalam@gmail.com>
2024-10-10 06:21:45 +0530

    k8s_aws_1.2 argocdcd1

---
+++ shellscripts/clear_hostkeys.sh
@@ -0,0 +1,4 @@
+ssh-keygen -f '/home/ubuntu/.ssh/known_hosts' -R 'master'
+ssh-keygen -f '/home/ubuntu/.ssh/known_hosts' -R 'worker'
+ssh-keygen -f '/home/ubuntu/.ssh/known_hosts' -R '172.131.0.37'
+ssh-keygen -f '/home/ubuntu/.ssh/known_hosts' -R '172.131.0.38'
