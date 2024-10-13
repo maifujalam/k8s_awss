a3dff8988f3174e8c8868f54f515330a2b8c07d2
maifujalam <sk.maifujalam@gmail.com>
2024-10-13 11:15:23 +0530

    k8s_aws_1.2 argocdcd1

--- terraform/iam/hosted-zone/lets-encrypt/main.tf
+++ terraform/iam/hosted-zone/lets-encrypt/main.tf
@@ -1,5 +1,9 @@
 resource "aws_iam_policy" "lets-encrypt" {
-  name   = "lets-encrypt_policy"
+  name   = "lets-encrypt-policy"
   path   = "/"
   policy = file("lets-encrypt-policy.json")
+}
+resource "aws_iam_policy" "lets-encrypt-policy" {
+  name = "RoleForCertManager"
+  policy = file("trust-policy.json")
 }
