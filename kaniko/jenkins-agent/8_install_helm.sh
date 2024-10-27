3717528cfcc174cc5e6fab61541bf59438d02308
maifujalam <sk.maifujalam@gmail.com>
2024-10-27 22:26:06 +0530

    k8s_aws_1.2 jenkins3

--- kaniko/jenkins-agent/8_install_helm.sh
+++ kaniko/jenkins-agent/8_install_helm.sh
@@ -8,3 +8,8 @@ fi
 tar zxf helm-$VERSION-linux-amd64.tar.gz
 cp linux-amd64/helm /usr/local/bin/
 rm -frv linux-amd64
+
+helm plugin install https://github.com/hypnoglow/helm-s3.git --version 0.16.2
+#helm repo add public-helm-repository s3://public-helm-repository-20231023095000508500000001
+#helm repo update
+#helm s3 reindex public-helm-repository;
---
+++ kaniko/jenkins-agent/install-terraform.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
