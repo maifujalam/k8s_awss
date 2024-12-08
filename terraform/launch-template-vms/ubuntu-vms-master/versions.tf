c80a689c1b6d7cc75b334789e562478b036404c1
maifujalam <sk.maifujalam@gmail.com>
2024-12-08 09:50:41 +0530

    k8s v1.31.3

--- terraform/launch-template-vms/ubuntu-vms-master/versions.tf
+++ terraform/launch-template-vms/ubuntu-vms-master/versions.tf
@@ -10,5 +10,6 @@ terraform {
     bucket = "terraform-backend-alam2"
     key = "terraform-state/k8s_aws/launch-template-vms/ubuntu-vms-master"   # Hare our state files get stored for this object.
     region = "ap-south-1"
+    profile = "default"
   }
 }
